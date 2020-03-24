e206a58b783dc7d09152ec8c59d157d4a5a7a864
manuelnagele <manuel.nagele@gmail.com>
2020-03-24 19:58:30 +0100

    improved fetching of geodata for countries

--- coronaparser.py
+++ coronaparser.py
@@ -2,6 +2,7 @@
 import pycountry
 import requests
 import json
+from geodatahelper import get_geodata_for_country
 
 corona_api_url2 = "https://corona.lmao.ninja/countries"
 corona_api_url = "https://coronavirus-19-api.herokuapp.com/countries"
@@ -16,54 +17,11 @@ updated_corona_data = []
 
 for country in raw_corona_data:
     country_name = country['country']
-
-    if country_name == 'UK':
-        country['country_code'] = 'GBR'
+    
+    if country_name == "Diamond Princess":
         continue
 
-    if country_name == 'Laos':
-        country['country_code'] = 'LAO'
-        continue
-
-    if country_name == 'S. Korea':
-        country['country_code'] = 'KOR'
-        continue
-
-    if country_name == 'St. Barth':
-        country_name = 'Saint Barthelemy'
-
-    if country_name == 'Diamond Princess':
-        continue
-
-    if country_name == 'UAE':
-        country['country_code'] = 'UAE'
-        continue
-
-    if country_name == 'Faeroe Islands':
-        country['country_code'] = 'FRO'
-        continue
-
-    if country_name == 'Ivory Coast':
-        country['country_code'] = 'CIV'
-        continue
-
-    if country_name == 'Channel Islands':
-        country['country_code'] = 'GSY'
-        continue
-
-    if country_name == 'DRC':
-        country['country_code'] = 'DRC'
-        continue
-
-    if country_name == 'St. Vincent Grenadines':
-        country['country_code'] = 'VCT'
-        continue
-
-    if country_name == 'U.S. Virgin Islands':
-        country['country_code'] = 'VIR'
-        continue
-
-    country['country_code'] = pycountry.countries.search_fuzzy(country_name)[0].alpha_3
+    country['country_code'] = get_geodata_for_country(country_name)
     updated_corona_data.append(country)
 
 print(json.dumps(updated_corona_data))
--- geodatahelper.py
+++ geodatahelper.py
@@ -2,9 +2,19 @@ import json
 
 # import geodata
 with open("./AT-GeoData.json") as json_file:
-        geodata = json.load(json_file)
+    at_geodata = json.load(json_file)
+
+with open("./WORLD-GeoData.json") as json_file:
+    world_geodata = json.load(json_file)
 
 def get_geodata_for_district(key):
-    for district in geodata:
+    for district in at_geodata:
         if district['name'] == key:
             return district['geodata']
+
+def get_geodata_for_country(key):
+    for country in world_geodata:
+        if country['country_name'] == key:
+            return country['country_code']
+
+print(get_geodata_for_country("UK"))
